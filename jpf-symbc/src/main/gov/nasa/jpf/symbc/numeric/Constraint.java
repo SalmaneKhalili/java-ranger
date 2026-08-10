20e999d8bfae10d5be48a572daf9cc85474d0467
salmane <salmajo2001@gmail.com>
2026-08-10 08:06:55 +0100

    style(fp): realign Comparator/Constraint to original commit bytes; keep equals null-guard + evaluate Javadoc

--- jpf-symbc/src/main/gov/nasa/jpf/symbc/numeric/Constraint.java
+++ jpf-symbc/src/main/gov/nasa/jpf/symbc/numeric/Constraint.java
@@ -37,6 +37,7 @@
 
 package gov.nasa.jpf.symbc.numeric;
 
+import gov.nasa.jpf.symbc.numeric.Comparator;
 import java.util.Map;
 
 public abstract class Constraint implements Comparable<Constraint> {
@@ -47,6 +48,7 @@ public abstract class Constraint implements Comparable<Constraint> {
   private final Expression right;
 
   public Constraint and;
+
 
   public Constraint(Expression l, Comparator c, Expression r) {
     left = l;
@@ -122,10 +124,12 @@ public abstract class Constraint implements Comparable<Constraint> {
   }
 
   public boolean equals(Object o) {
-    if (o == null){
-      return false;
-    }
-    if (!(o instanceof Constraint)) {
+
+	  if (o == null){
+		  return false;
+	  }
+
+	  if (!(o instanceof Constraint)) {
       return false;
     }
 
@@ -176,13 +180,12 @@ public abstract class Constraint implements Comparable<Constraint> {
 		}
 		return r;
 	}
-  
+
   public String toString() {
     if (right == null)
       return comp.toString() + "(" + left.toString() + ")"
           + ((and == null) ? "" : " &&\n" + and.toString());
     return left.toString() + comp.toString() + right.toString()
-        //+ ((and == null) ? "" : " && " + and.toString()); -- for specialization
         + ((and == null) ? "" : " &&\n" + and.toString());
   }
 
@@ -193,7 +196,7 @@ public abstract class Constraint implements Comparable<Constraint> {
       }
       return c;
   }
-  
+
 //JacoGeldenhuys
 	public void accept(ConstraintExpressionVisitor visitor) {
 		visitor.preVisit(this);
